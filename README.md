d9bc415b157834d502575918102ca248beef7acf
John Bintz <27256+johnbintz@users.noreply.github.com>
2021-03-28 16:48:03 -0400

    add this amazing code

---
+++ README.md
@@ -0,0 +1,17 @@
+# Final Fantasy Save File to Password Generator
+
+A dumb thing I built for
+[a Rabbit with 1000 Repos blog post](https://rabbitwith1000repos.com/read/nesstate)
+about restoring state in NES games that will turn a Final Fantasy save file
+used with emulators into a Mega Man 3-like password grid.
+
+Try it with [one of the save files listed here](https://fantasyanime.com/finalfantasy/ff/ffsaves.htm).
+Then imagine trying to type all that in with an NES controller, let alone write it down in a nearly-used
+school notebook so you can recreate it later. Aren't you glad that memory management
+controllers that could swap in WRAM storage were made?
+
+All the info about the offsets in the file came from
+[this GameFAQ](https://gamefaqs.gamespot.com/nes/522595-final-fantasy/faqs/12630)
+and, upon pulling the .sav file into a hex editor, seemed to be correct.
+
+Enjoy!
---
+++ to_mm3_selector.rb
@@ -0,0 +1,115 @@
+#!/usr/bin/env ruby
+
+require 'pp'
+require 'base64'
+
+BLOCKS = [
+  0x0000..0x01EF,
+  0x0300..0x040F
+]
+
+WIDTH = 38
+
+output = []
+current_row = []
+
+string = ""
+
+File.open('Final Fantasy-GodMode.sav') do |fh|
+  fh.each_byte do |byte|
+    pos = fh.pos
+
+    if BLOCKS.any? { |b| b.include?(pos) }
+      string << byte
+
+      parts = byte.to_s(4).split(//)
+
+      parts.each do |part|
+        current_row << part
+
+        if current_row.length == WIDTH
+          output << current_row
+          current_row = []
+        end
+      end
+    end
+  end
+end
+
+puts Base64.urlsafe_encode64(string).tap { |l| puts l.length }
+
+
+exit
+
+loop do
+  break if current_row.length == WIDTH
+
+  current_row << rand(4)
+end
+
+output << current_row
+
+File.open("output.html", "w") do |out|
+  out.puts(<<-HTML)
+<html lang="en">
+  <head>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/js/all.min.js" defer></script>
+    <meta charset="UTF-8">
+    <style type="text/css">
+body, html {
+  background-color: black;
+}
+
+table {
+  border-radius: 8px;
+  border: 6px solid white;
+  margin: 16px;
+}
+
+table td {
+  padding: 8px;
+  border: 1px solid white;
+}
+
+.value-1 * {
+  color: white;
+}
+
+.value-2 * {
+  color: mediumorchid;
+}
+
+.value-3 * {
+  color: lime;
+}
+    </style>
+  </head>
+  <body>
+    <table cellspacing="0">
+HTML
+
+  output.each do |row|
+    out.puts(<<-HTML)
+      <tr>
+    HTML
+
+    row.each do |value|
+      if value == "0"
+        out.puts %{<td class="value-#{value}">&nbsp;</td>}
+      else
+        out.puts %{<td class="value-#{value}"><i class="fas fa-circle"></i></td>}
+      end
+    end
+
+    out.puts(<<-HTML)
+      </tr>
+    HTML
+  end
+
+  out.puts(<<-HTML)
+    </table>
+  </body>
+</html>
+HTML
+end
+
